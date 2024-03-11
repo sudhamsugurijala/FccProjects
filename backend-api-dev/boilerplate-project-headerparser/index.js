d7a723224e744d514898cdaf1f957b19d9a0b9f2
Sudhamsu <gurijalasudhamsu2000@gmail.com>
2024-03-11 23:51:58 +0530

    Implements endpoint for headerparser project

--- backend-api-dev/boilerplate-project-headerparser/index.js
+++ backend-api-dev/boilerplate-project-headerparser/index.js
@@ -24,6 +24,14 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function(req, res) {
+  let response = {};
+  response.ipaddress = req.ip;
+  response.language = req.headers['accept-language'];
+  response.software = req.headers['user-agent'];
+  res.send(response);
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
