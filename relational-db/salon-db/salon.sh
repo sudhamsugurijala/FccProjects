78829c61bbe72456872762aee9e05986c735a30d
Sudhamsu <gurijalasudhamsu2000@gmail.com>
2024-04-20 19:19:00 +0530

    Completes salon-db project

--- relational-db/salon-db/salon.sh
+++ relational-db/salon-db/salon.sh
@@ -11,14 +11,29 @@ echo 'Welcome to My Salon, how can I help you?'
 showServices
 read SERVICE_ID_SELECTED
 
-while [ $SERVICE_ID_SELECTED -lt 1 -o $SERVICE_ID_SELECTED -gt 3 ];
+while [[ $SERVICE_ID_SELECTED =~ [^0-9] ]] || [ $SERVICE_ID_SELECTED -lt 1 -o $SERVICE_ID_SELECTED -gt 3 ];
 do
   echo -e '\nI could not find that service. What would you like today?'
   showServices
   read SERVICE_ID_SELECTED
 done
 
+SERVICE_NAME="$($PSQL "SELECT name FROM services WHERE service_id='$SERVICE_ID_SELECTED'")"
 echo -e "\nWhat's your phone number?"
 read CUSTOMER_PHONE
 
-echo -e "\n$($PSQL "SELECT COUNT(*) FROM customers WHERE phone='$CUSTOMER_PHONE'")"
+CUSTOMER_NAME="$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'")"
+
+if [[ "$CUSTOMER_NAME" == "" ]]; then
+  echo -e "\nI don't have a record for that phone number, what's your name?"
+  read CUSTOMER_NAME
+  $PSQL "INSERT INTO customers (name, phone) VALUES ('$CUSTOMER_NAME','$CUSTOMER_PHONE')"
+fi
+
+CUSTOMER_ID="$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")"
+
+echo -e "\nWhat time would you like your cut, $CUSTOMER_NAME?"
+read SERVICE_TIME
+$PSQL "INSERT INTO appointments (customer_id, service_id, time) VALUES ('$CUSTOMER_ID','$SERVICE_ID_SELECTED','$SERVICE_TIME')"
+
+echo -e "\nI have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
