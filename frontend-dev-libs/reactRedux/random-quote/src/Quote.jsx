02ec0e8845fe81de852bad426c4fc89633391a6a
Sudhamsu <gurijalasudhamsu2000@gmail.com>
2024-06-12 01:10:40 +0530

    Adds fa icons

--- frontend-dev-libs/reactRedux/random-quote/src/Quote.jsx
+++ frontend-dev-libs/reactRedux/random-quote/src/Quote.jsx
@@ -5,16 +5,16 @@ class Quote extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      quote: 'This is the first quote',
-      author: 'Myself',
+      quote: '',
+      author: '',
       quotes: []
     }
   }
 
   componentDidMount() {
     let component = this;
-    let index = Math.floor(Math.random()*15);
-    fetch("https://type.fit/api/quotes").then(function(response) {
+    let index = Math.floor(Math.random() * 15);
+    fetch("https://type.fit/api/quotes").then(function (response) {
       return response.json();
     }).then((data) => {
       component.setState({
@@ -26,7 +26,7 @@ class Quote extends React.Component {
   }
 
   fetchQuote = () => {
-    let index = Math.floor(Math.random()*15);
+    let index = Math.floor(Math.random() * 15);
     let text = this.state.quotes.at(index).text;
     let author = this.state.quotes.at(index).author;
     this.setState({
@@ -37,12 +37,23 @@ class Quote extends React.Component {
 
   render() {
     return (
-    <div id="quote-box">
-        <div id="text">{this.state.quote}</div>
-        <div id="author">{this.state.author}</div>
-        <a href="https://www.twitter.com/intent/tweet" id="tweet-quote"><button>Tweet Quote</button></a>
-        <button id="new-quote" onClick={this.fetchQuote}>New Quote</button>
-    </div>
+      <div id="quote-box">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" />
+        <div class="quote-text">
+          <i class="fas fa-quote-left"> </i><span id="text"> {this.state.quote}</span>
+        </div>
+        <div class="quote-author">
+          - <span id="author">{this.state.author}</span>
+        </div>
+        <div class="buttons">
+          <button class="button" id="tweet-quote" title="Tweet this quote!" target="_blank">
+            <i class="fab fa-twitter"></i>  Tweet quote
+          </button>
+          <button class="button" id="new-quote" onClick={this.fetchQuote}>
+          <i class="fa fa-play"></i>  New quote
+          </button>
+        </div>
+      </div>
     );
   }
 };
